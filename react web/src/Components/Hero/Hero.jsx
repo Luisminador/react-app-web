00840a2e55dd488cdcc87de84094c14c085c4eb8
Luisminador <luis.axel98@outlook.com>
2024-10-17 22:05:17 +0200

    testing commit

--- react web/src/Components/Hero/Hero.jsx
+++ react web/src/Components/Hero/Hero.jsx
@@ -4,7 +4,11 @@ import './Hero.css'
 const Hero = () => {
   return (
     <div className='hero'>
-      
+      <div className="hero-text">
+        <h1>We ensure better education for a better world</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas magni animi assumenda. Dolorum reiciendis accusamus eaque. Itaque repellendus labore quod nihil laborum, excepturi nulla iusto autem possimus.</p>
+        <button className="btn"></button>
+      </div>
     </div>
   )
 }
